eb615625395fb9e42ae87b5d54a503bd7bc32127
trellyvision <68975400+trellyvision@users.noreply.github.com>
2024-01-14 16:58:09 -0500

    Update data.js

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ var data = [
         "title": "Spreading the News | Wake Forest Women's Basketball Goes Dancing",
         "type": "video",
         "medium": "Motion Graphics",
-        "genre": "Sports",
+        "genre": "NCAA",
         "thumbnailFilename": "http-::i3.ytimg.com:vi:Om6ziZ6rDAA:maxresdefault.jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=Om6ziZ6rDAA",
         "dateCreated": "2021-03-08T05:00:00.000Z"
@@ -11,8 +11,8 @@ var data = [
     {
         "title": "Women's Basketball Game Recap | Wake vs. UNC ACC Tourney",
         "type": "video",
-        "medium": "Portfolio",
-        "genre": "Game Recap",
+        "medium": "Videography",
+        "genre": "NCAA",
         "thumbnailFilename": "WBB_UNC_ACCTourney_GameRecap.jpeg",
         "youtubeUrl": "https://www.youtube.com/watch?v=xC8RV5YOy14",
         "dateCreated": "2021-03-04T05:00:00.000Z"
@@ -21,34 +21,16 @@ var data = [
         "title": "Ivana Raca In Her Bag Series (Clemson)",
         "type": "video",
         "medium": "Motion Graphics",
-        "genre": "Sports",
+        "genre": "NCAA",
         "thumbnailFilename": "http-::i3.ytimg.com:vi:YEIOOA68zKA:hqdefault.jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=YEIOOA68zKA",
         "dateCreated": "2021-02-14T05:00:00.000Z"
     },
     {
-        "title": "Women's Basketball Game Recap | 2/14/21 Wake vs Clemson",
-        "type": "video",
-        "medium": "Portfolio",
-        "genre": "Game Recap",
-        "thumbnailFilename": "WBB_Clemson_GameRecap.jpeg",
-        "youtubeUrl": "https://www.youtube.com/watch?v=EOkFEBf56B4",
-        "dateCreated": "2021-02-14T05:00:00.000Z"
-    },
-    {
-        "title": "Women's Basketball Game Recap | 2/11/21 Pitt vs. Wake",
-        "type": "video",
-        "medium": "Portfolio",
-        "genre": "Game Recap",
-        "thumbnailFilename": "WBB_Pitt_GameRecap.jpeg",
-        "youtubeUrl": "https://www.youtube.com/watch?v=16c2Fdu0xGY",
-        "dateCreated": "2021-02-11T05:00:00.000Z"
-    },
-    {
         "title": "Gina Conti 2k Take Over",
         "type": "video",
         "medium": "Motion Graphics",
-        "genre": "Sports",
+        "genre": "NCAA",
         "thumbnailFilename": "http-::i3.ytimg.com:vi:iNfKLfd5Dmw:hqdefault.jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=iNfKLfd5Dmw",
         "dateCreated": "2021-01-28T05:00:00.000Z"
@@ -56,8 +38,8 @@ var data = [
     {
         "title": "Women's Basketball Game Recap | 1/17/21 Wake vs. VT",
         "type": "video",
-        "medium": "Portfolio",
-        "genre": "Game Recap",
+        "medium": "Videography",
+        "genre": "NCAA",
         "thumbnailFilename": "WBB_VT_GameRecap.jpeg",
         "youtubeUrl": "https://www.youtube.com/watch?v=ehCnDNhpoD4",
         "dateCreated": "2021-01-17T05:00:00.000Z"
@@ -66,7 +48,7 @@ var data = [
         "title": "Ivana Raca 2k Takeover Mode",
         "type": "video",
         "medium": "Motion Graphics",
-        "genre": "Sports",
+        "genre": "NCAA",
         "thumbnailFilename": "http-::i3.ytimg.com:vi:Uy1PCHOP_Wc:hqdefault.jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=Uy1PCHOP_Wc",
         "dateCreated": "2021-01-10T05:00:00.000Z"
@@ -74,8 +56,8 @@ var data = [
     {
         "title": "Wake Women's Basketball Intro Video",
         "type": "video",
-        "medium": "Portfolio",
-        "genre": "Sports",
+        "medium": "Videography",
+        "genre": "NCAA",
         "thumbnailFilename": "http-::i3.ytimg.com:vi:kRZbxpbUQtY:maxresdefault.jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=kRZbxpbUQtY",
         "dateCreated": "2020-12-10T05:00:00.000Z"
@@ -84,7 +66,7 @@ var data = [
         "title": "Women's Basketball Team Intro Motion Graphic",
         "type": "video",
         "medium": "Motion Graphics",
-        "genre": "Sports",
+        "genre": "NCAA",
         "thumbnailFilename": "http-::i3.ytimg.com:vi:w_blU6o_L2w:maxresdefault.jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=w_blU6o_L2w",
         "dateCreated": "2020-12-10T05:00:00.000Z"
@@ -93,7 +75,7 @@ var data = [
         "title": "Nevaeh Brown Pregame Mix",
         "type": "video",
         "medium": "Motion Graphics",
-        "genre": "Sports",
+        "genre": "NCAA",
         "thumbnailFilename": "http-::i3.ytimg.com:vi:QlYX31f3gZ8:maxresdefault.jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=QlYX31f3gZ8",
         "dateCreated": "2020-12-03T05:00:00.000Z"
@@ -103,7 +85,7 @@ var data = [
         "slug": "WakeWomensBasketballBeachBubbleTourney",
         "type": "photo",
         "medium": "Photography",
-        "genre": "Sports",
+        "genre": "NCAA",
         "thumbnailFilename": "20201129_WBB_IvanaRaca_MissouriSt._12.jpg",
         "photoFilenames": "20201127_WBB_AlexScurggs_Arkansas_1.jpg,20201127_WBB_AlexScurggs_Arkansas_2.jpg,20201127_WBB_ErinDickerson_Arkansas_3.jpg,20201127_WBB_ErinDickerson_Arkansas_6.jpg,20201127_WBB_GinaConti_Arkansas_4.jpg,20201127_WBB_GinaConti_Arkansas_8.jpg,20201127_WBB_IvanaRaca_Arkansas_4.jpg,20201127_WBB_JenaReady_Arkansas_1.jpg,20201127_WBB_JenaReady_Arkansas_2.jpg,20201127_WBB_JenHoover_Arkansas_1.jpg,20201127_WBB_JenHoover_Arkansas_4.jpg,20201127_WBB_KaiaHarrison_Arkansas_4.jpg,20201127_WBB_NevaehBrown_Arkansas_7.jpg,20201127_WBB_NevaehBrown_Arkansas_12.jpg,20201127_WBB_OliviaSummiel_Arkansas_8.jpg,20201129_WBB_GinaConti_MissouriSt._8.jpg,20201129_WBB_IvanaRaca_MissouriSt._3.jpg,20201129_WBB_IvanaRaca_MissouriSt._6.jpg,20201129_WBB_IvanaRaca_MissouriSt._8.jpg,20201129_WBB_IvanaRaca_MissouriSt._9.jpg,20201129_WBB_IvanaRaca_MissouriSt._12.jpg,20201129_WBB_JewelSpear_MissouriSt._4.jpg,20201129_WBB_JewelSpear_MissouriSt._7.jpg,20201129_WBB_KaiaHarrison_MissouriSt._3.jpg,20201129_WBB_KaiaHarrison_MissouriSt._4.jpg,20201129_WBB_KaiaHarrison_MissouriSt._6.jpg,20201129_WBB_KaiaHarrison_MissouriSt._7.jpg,20201129_WBB_OliviaSummiel_MissouriSt._1.jpg,20201129_WBB_OliviaSummiel_MissouriSt._3.jpg",
         "dateCreated": "2020-11-29T05:00:00.000Z"
@@ -112,7 +94,7 @@ var data = [
         "title": "Women's Basketball Media Day BTS",
         "type": "video",
         "medium": "Motion Graphics",
-        "genre": "Sports",
+        "genre": "NCAA",
         "thumbnailFilename": "http-::i3.ytimg.com:vi:bjP7D9aJ39I:maxresdefault.jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=bjP7D9aJ39I",
         "dateCreated": "2020-11-01T04:00:00.000Z"
@@ -120,8 +102,8 @@ var data = [
     {
         "title": "Field Hockey Game Recap | 10/22/2020 Duke vs. Wake",
         "type": "video",
-        "medium": "Portfolio",
-        "genre": "Game Recap",
+        "medium": "Videography",
+        "genre": "NCAA",
         "thumbnailFilename": "Wake_Duke_FieldHockey_GameRecap_Thumbnail.jpeg",
         "youtubeUrl": "https://www.youtube.com/watch?v=fWHW-mvqvk4",
         "dateCreated": "2020-10-22T04:00:00.000Z"
@@ -130,7 +112,7 @@ var data = [
         "title": "Gina Conti Watching Ivana Raca Highlights",
         "type": "video",
         "medium": "Motion Graphics",
-        "genre": "Sports",
+        "genre": "NCAA",
         "thumbnailFilename": "http-::i3.ytimg.com:vi:8mzuf8l2Zko:maxresdefault.jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=8mzuf8l2Zko",
         "dateCreated": "2020-10-15T04:00:00.000Z"
@@ -138,8 +120,8 @@ var data = [
     {
         "title": "Alicia + Derek Wedding ",
         "type": "video",
-        "medium": "Portfolio",
-        "genre": "Weddings",
+        "medium": "Videography",
+        "genre": "Weddings ",
         "thumbnailFilename": "Alicia + Derek Wedding .jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=jPA0fCiafNw",
         "dateCreated": "2020-10-10T04:00:00.000Z"
@@ -147,8 +129,8 @@ var data = [
     {
         "title": "Savage Roasters Commercial",
         "type": "video",
-        "medium": "Portfolio",
-        "genre": "Sports",
+        "medium": "Videography",
+        "genre": "NCAA",
         "thumbnailFilename": "SavageRoasters_thumbnail.jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=DOLk9qG_5Iw",
         "dateCreated": "2020-10-03T04:00:00.000Z"
@@ -157,7 +139,7 @@ var data = [
         "title": "Field Hockey Gameday Promo | UNC vs. Wake",
         "type": "video",
         "medium": "Motion Graphics",
-        "genre": "Sports",
+        "genre": "NCAA",
         "thumbnailFilename": "http-::i3.ytimg.com:vi:SNi0GiJrB-E:maxresdefault.jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=SNi0GiJrB-E",
         "dateCreated": "2020-09-20T04:00:00.000Z"
@@ -166,7 +148,7 @@ var data = [
         "title": "Wake Forest Women's Soccer Hype Video",
         "type": "video",
         "medium": "Motion Graphics",
-        "genre": "Sports",
+        "genre": "NCAA",
         "thumbnailFilename": "WSOC_HypeVid_thumbnail.jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=P1QDfNfsnj8",
         "dateCreated": "2020-09-10T04:00:00.000Z"
@@ -174,8 +156,8 @@ var data = [
     {
         "title": "Football BTS Video",
         "type": "video",
-        "medium": "Portfolio",
-        "genre": "Sports",
+        "medium": "Videography",
+        "genre": "NCAA",
         "thumbnailFilename": "Football_BTS.jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=FI8DgKaTPto",
         "dateCreated": "2020-09-05T04:00:00.000Z"
@@ -183,8 +165,8 @@ var data = [
     {
         "title": "Wake Forest Men's Soccer Media Day BTS",
         "type": "video",
-        "medium": "Portfolio",
-        "genre": "Sports",
+        "medium": "Videography",
+        "genre": "NCAA",
         "thumbnailFilename": "MSOC_BTS.jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=PMJMn_TuJ6c",
         "dateCreated": "2020-08-14T04:00:00.000Z"
@@ -192,8 +174,8 @@ var data = [
     {
         "title": "Adams Wedding | Wedding Trailer // Most Epic Wedding Moments",
         "type": "video",
-        "medium": "Portfolio",
-        "genre": "Weddings",
+        "medium": "Videography",
+        "genre": "Weddings ",
         "thumbnailFilename": "5f2a048c5bde44406bd68641_Wedding_Trailer_thumbnail.jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=0CKyzAaS1KA",
         "dateCreated": "2020-08-02T04:00:00.000Z"
@@ -201,8 +183,8 @@ var data = [
     {
         "title": "Maria Whitehead | Fight for Five",
         "type": "video",
-        "medium": "Portfolio",
-        "genre": "Sports",
+        "medium": "Videography",
+        "genre": "NCAA",
         "thumbnailFilename": "5f178808bd94e30f7fc1907e_Maria_thumbnail.jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=q-KWNEZoJ78",
         "dateCreated": "2020-05-08T04:00:00.000Z"
@@ -211,7 +193,7 @@ var data = [
         "title": "Wake The Nation!",
         "type": "video",
         "medium": "Motion Graphics",
-        "genre": "Sports",
+        "genre": "NCAA",
         "thumbnailFilename": "5f17861a8ee5d75748d741c7_WakeTheNation_thumbnail.jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=WO6hGL14UAU",
         "dateCreated": "2020-02-27T05:00:00.000Z"
@@ -221,7 +203,7 @@ var data = [
         "slug": "ITACarolinaRegionals",
         "type": "photo",
         "medium": "Photography",
-        "genre": "Sports",
+        "genre": "NCAA",
         "thumbnailFilename": "5f2231c4820e0b5b586ed2e9_Lexi%2BPeyton5.png",
         "photoFilenames": "5f222db0f4f1e88101ca4982_Anna_Brylin7.png,5f222db063ca0903d510c10c_Anna_Brylin8.png,5f222deb68fd8efce8bca08c_Anna_Brylin.png,5f222e5700b770390599a801_Anna_Campana5.png,5f222e7887c2cd19745cfb86_Anna_Campana6.png,5f222eb65a32ac1edd1782b0_Carolyn.png,5f222ef386393981fe020740_Carolyn2.png,5f222f3aaebf57eea89fb15b_Eliza3.png,5f222f625a32acef3017849a_Eliza4.png,5f222fd06eac47c5c99c3211_Eliza5.png,5f222ff6ba48deabfa559fd1_Eliza6.png,5f223026bfc5c6ca977aa672_Lexi%2BPeyton3.png,5f22304e6a43cf0da3e688ac_Lexi2.png,5f22307db57c7a1091c72224_Lexi3.png,5f2230b363ca09cf4210c52e_Lexi8.png,5f2230dd53fb855a02ce66ca_Lexi9.png,5f22313353fb858581ce67b0_MC.png,5f2231c4820e0b5b586ed2e9_Lexi%2BPeyton5.png,5f2232008f5d4c23d82fd661_Peyton2.png,5f223221abe392768700bbe4_Peyton3.png,5f2232598f5d4c44d02fd6d3_Saby2.png,5f22327acb4fb31d42441552_Saby3.png,5f2232c26eac4704ae9c3a36_Saby4.png,5f2232e4c3cfa01f781843ee_Saby5.png,5f223300edf09d8fd6287d9e_Saby7.png",
         "dateCreated": "2019-10-28T04:00:00.000Z"
@@ -239,7 +221,7 @@ var data = [
     {
         "title": "Caroline + Chris Wedding",
         "type": "video",
-        "medium": "Portfolio",
+        "medium": "Videography",
         "genre": "Weddings",
         "thumbnailFilename": "5f0339305340e3db953bf21f_Chris%2BCaroline_Thumbnail.jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=R_O8aOMJcBM",
@@ -248,7 +230,7 @@ var data = [
     {
         "title": "Heather + Kevin's Wedding",
         "type": "video",
-        "medium": "Portfolio",
+        "medium": "Videography",
         "genre": "Weddings",
         "thumbnailFilename": "5d719e60b89ebacf776d9d93_maxresdefault%20(11).jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=jtA0rU3oQRk",
@@ -259,7 +241,7 @@ var data = [
         "slug": "NBAAllStarRisingStarsPractice2019",
         "type": "photo",
         "medium": "Photography",
-        "genre": "Sports",
+        "genre": "NCAA",
         "thumbnailFilename": "5d9e43ba7c4b2508fb74ec54_NBA_AllStar_TV11-min.jpg",
         "photoFilenames": "5d9e43d2eb3f2ac066d65075_NBA_AllStar_TV1-min.jpg,5d9e43d2138094617e2a7a95_NBA_AllStar_TV2-min.jpg,5d9e43e0cf6005088502879d_NBA_AllStar_TV3-min.jpg,5d9e43df7c4b25700474ece0_NBA_AllStar_TV4-min.jpg,5d9e43d27c4b250bb174ecc8_NBA_AllStar_TV5-min.jpg,5d9e43d213809476062a7a94_NBA_AllStar_TV6-min.jpg,5d9e43dd853df232289bca6a_NBA_AllStar_TV7-min.jpg,5d9e43d27c4b25a04e74ecc7_NBA_AllStar_TV8-min.jpg,5d9e43dd853df2eecf9bca6c_NBA_AllStar_TV9-min.jpg,5d9e43d213809402372a7a96_NBA_AllStar_TV10-min.jpg,5d9e43ba7c4b2508fb74ec54_NBA_AllStar_TV11-min.jpg,5d9e43dbcf6005104002876c_NBA_AllStar_TV12-min.jpg,5d9e43db83c36efe053166e9_NBA_AllStar_TV13-min.jpg,5d9e43d2eb3f2a9e66d65076_NBA_AllStar_TV14-min.jpg,5d9e43d283c36e27c73166b7_NBA_AllStar_TV15-min.jpg,5d9e43dd83c36ed7c53166ee_NBA_AllStar_TV16-min.jpg,5d9e43dd83c36e38ef3166ed_NBA_AllStar_TV17-min.jpg,5d9e43ddeb3f2a6557d650da_NBA_AllStar_TV18-min.jpg",
         "dateCreated": "2019-02-01T05:00:00.000Z"
@@ -269,7 +251,7 @@ var data = [
         "slug": "WakevZionWilliamson",
         "type": "photo",
         "medium": "Photography",
-        "genre": "Sports",
+        "genre": "NCAA",
         "thumbnailFilename": "5f22b9105f105b27d0d382fd_Brandon_Takeoff.png",
         "photoFilenames": "5f22b870a6a923369dfb7e45_Bench_Cheering_2.png,5f22b8a529c6adbb077129b2_Bench_Cheering.png,5f22b9105f105b27d0d382fd_Brandon_Takeoff.png,5f22b916a6a92306affb8039_Brandon%2BBarrett_Pullup.png,5f22b945cf522b127d3dee5b_Chaundee_Drive_Barrett.png,5f22b972caf168560e0ecc34_Chaundee_Layup.png,5f22b945cf522b127d3dee5b_Chaundee_Drive_Barrett.png,5f22bb76aeff43b4d8d6a9b1_Chaundee_TurnUp.png,5f22bb76364f6f21e3bcfe7c_Isaiah_Pullup.png,5f22ba1e82f53f57f963547f_Screen%20Shot%202020-07-30%20at%208.16.13%20AM.png,5f22bb7529ca592737bf76b5_Isaiah%2BZion.png,5f22bb75caf168ad0f0ed093_Jaylen_OverBarrett.png,5f22bb75f90ddfb3cc2713d8_Jaylen%2BZion_Layup.png,5f22bb7519e812b9b9288657_Sharone_Layup.png,5f22bb7540a63e34eae9d240_Sharone_Layup_Sepia%20Tone.png,5f22bb75a9914e6433893c62_Sharone_Layup_White.png,5f22bb74c703fa02d0ffde74_Team_Huddle.png",
         "dateCreated": "2019-01-08T05:00:00.000Z"
@@ -279,7 +261,7 @@ var data = [
         "slug": "WomensTennisStagePhotos",
         "type": "photo",
         "medium": "Photography",
-        "genre": "Sports",
+        "genre": "NCAA",
         "thumbnailFilename": "5f2226753166e46a0c5bba6a_Anna1631.png",
         "photoFilenames": "5f22264fcb4fb395fc43ff03_Anna1308.png,5f2226753166e46a0c5bba6a_Anna1631.png,5f22269f3166e417175bba70_Anna1635.png,5f2226ca87c2cd02ff5ce177_Chand1935.png,5f2226eebfc5c6fb967a8279_chandler1599.png,5f2227155a32acd2cc1774e9_chandler01613.png,5f222736fd51880ed9ad4c26_Eliza1519.png,5f222755f9d1b10b44e5bd3e_Eliza1526.png,5f22277607bc7fb1c36832eb_Emma1313.png,5f2227935a32ac768e177552_Emma1799.png,5f2227bb8f5d4c66a52fc8b0_Jojo1469.png,5f2227db00b7705470999da8_Jojo1589.png,5f2227fe68e45978358861da_Lexi.png,5f22282d9cca2864aeb9e915_Lexi1374.png,5f2228618639393b8f01fbc0_Lexi1384.png,5f2228937682cb421b23a7c0_PeytonPesavento1.png,5f2228bf63183f0f6bffeba3_PeytonPesavento4.png,5f2228f063009c93d3ddcbd4_PeytonPesavento5.png,5f22291c63009c2360ddcc62_Saby1564.png,5f22295d7be7e639e9de876a_Saby1568.png",
         "dateCreated": "2018-10-25T04:00:00.000Z"
@@ -287,19 +269,23 @@ var data = [
     {
         "title": "WFU Madison Hammond More than an Athlete",
         "type": "video",
-        "medium": "Portfolio",
-        "genre": "Sports",
+        "medium": "Videography",
+        "genre": "NCAA",
         "thumbnailFilename": "5d719e7cf0cb5449a124eccb_hqdefault.jpg",
         "youtubeUrl": "https://www.youtube.com/watch?v=GtspJUGbiTw",
         "dateCreated": "2017-12-20T05:00:00.000Z"
+    },
+    {
+        "title": "Sacramento Kings Summer League Recap 2021",
+        "type": "video",
+        "medium": "Videography",
+        "genre": "NBA "
     }
 ];
 
-var defaultMedium = 'Portfolio';
+var defaultMedium = 'Sports';
 var defaultGenre = 'All';
 
 var filters = {
-    'Portfolio': ['All', 'Sports', 'Game Recap', 'Weddings', 'Social Content'],
-    'Motion Graphics': ['All', 'Sports'],
-    'Photography': ['All', 'Sports', 'Weddings']
+'Motion Graphics': ['All', 'NCAA'],'Videography': ['All', 'NCAA', 'Weddings ', 'Weddings', 'NBA '],'Photography': ['All', 'NCAA', 'Weddings']
 }
